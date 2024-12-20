99a01713d93db0fadc6afd755c263b951e2bca8d
Andrew Wallace <pdxbus@me.com>
2024-12-20 15:57:23 -0800

    Added mode to RGBLightColorCommand

--- Sources/BoostBLEKit/Command/RGBLightColorCommand.swift
+++ Sources/BoostBLEKit/Command/RGBLightColorCommand.swift
@@ -12,13 +12,21 @@ public struct RGBLightColorCommand: Command {
     
     public let portId: PortId
     public let color: Color
+    public let mode: UInt8
     
     public init(portId: PortId, color: Color) {
         self.portId = portId
         self.color = color
+        self.mode = 0x00
+    }
+    
+    public init(colorSensorPortId: PortId, color: Color) {
+        self.portId = colorSensorPortId
+        self.color = color
+        self.mode = 0x05
     }
     
     public var data: Data {
-        return Data([0x08, 0x00, 0x81, portId, 0x11, 0x51, 0x00, color.rawValue])
+        return Data([0x08, 0x00, 0x81, portId, 0x11, 0x51, mode, color.rawValue])
     }
 }
