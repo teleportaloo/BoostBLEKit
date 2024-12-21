916265a3c33295d8617fc1ac6023de40c1409547
Andrew Wallace <pdxbus@me.com>
2024-12-20 16:05:23 -0800

    added IMU ports from Technic hub

--- Sources/BoostBLEKit/Port.swift
+++ Sources/BoostBLEKit/Port.swift
@@ -10,13 +10,18 @@ import Foundation
 
 public typealias PortId = UInt8
 
-public enum Port {
+public enum Port: CaseIterable {
     
     case A
     case B
     case C
     case D
     case AB
+    case IMU_Tilt
+    case IMU_Accel
+    case IMU_Gyro
+    case IMU_TempA
+    case IMU_TempB
 }
 
 extension Port: CustomStringConvertible {
@@ -33,6 +38,16 @@ extension Port: CustomStringConvertible {
             return "D"
         case .AB:
             return "A and B"
+        case .IMU_Tilt:
+            return "IMU Tilt"
+        case .IMU_Accel:
+            return "IMU Accel"
+        case .IMU_Gyro:
+            return "IMU Gyro"
+        case .IMU_TempA:
+            return "IMU_TempA"
+        case .IMU_TempB:
+            return "IMU_TempB"
         }
     }
 }
